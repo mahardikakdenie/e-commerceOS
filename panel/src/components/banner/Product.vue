6a1cfc87c25fa25970341000eb9dc42989ec7ffb
MahardikaKDenie <dikamahar884@gmail.com>
2022-04-24 02:16:49 +0700

    setup pannel make dashboard add router vuex , vuex have some problme , fix later ,, ....

---
+++ panel/src/components/banner/Product.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="product overflow-hidden">
+    <div class="container d-flex align-items-center product__banner">
+      <div class="row gx-5">
+        <div class="col">
+          <div class="p-3" style="opacity: 0">Custom column padding</div>
+        </div>
+        <div class="col d-flex justify-content-end">
+          <div class="p-3">
+            <h1 class="text--product">Title Of Product</h1>
+            <p class="paragrap--product">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+              quos hic et ducimus officiis nemo recusandae a dolorum aliquam ab!
+            </p>
+            <button class="btn btn--product btn-lg">Explore</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ panel/src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <Product />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
+import Product from "@/components/banner/Product.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Product,
+    Navbar,
+  },
+};
+</script>
